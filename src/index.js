effbd88fb80e59c3bb7bbd3fdfd8da76882ce254
Olugbenga Ajayi <gbejula@gmail.com>
2022-02-24 18:25:55 +0100

    added reply, retweet, like and more componets

--- src/index.js
+++ src/index.js
@@ -8,13 +8,18 @@ function Tweet() {
       <Avatar />
       <div className='content'>
         <Author />
+        <Time />
         <Message />
+        <div className='buttons'>
+          <ReplyButton />
+          <RetweetButton />
+          <LikeButton />
+          <MoreOptionsButton />
+        </div>
       </div>
     </div>
   );
 }
-
-ReactDOM.render(<Tweet />, document.querySelector('#root'));
 
 function Avatar() {
   return (
@@ -33,8 +38,18 @@ function Message() {
 function Author() {
   return (
     <span className='author'>
-      <span className='name'>Your Name</span>
-      <span className='handle'>@handle</span>
+      <span className='name'>Tunde Onakoya</span>
+      <span className='handle'>@tonak</span>
     </span>
   );
 }
+
+const Time = () => <span className='time'>3h ago</span>;
+const ReplyButton = () => <i className='fa fa-reply reply-button' />;
+const RetweetButton = () => <i className='fa fa-retweet retweet-button' />;
+const LikeButton = () => <i className='fa fa-heart like-button' />;
+const MoreOptionsButton = () => (
+  <i className='fa fa-ellipsis-h more-options-button' />
+);
+
+ReactDOM.render(<Tweet />, document.querySelector('#root'));
